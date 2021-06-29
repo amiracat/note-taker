3393dcdd98238af9e26bb184c137de3f5c7a02f7
Amira Holden <amiraholden@gmail.com>
2021-06-28 21:54:32 -0500

    edited get request

--- server.js
+++ server.js
@@ -1,17 +1,67 @@
-const express = require("express");
+//dependencies
+const express = require('express');
 const fs = require('fs');
+const path = require('path');
 
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+//using local port 3011
+const PORT = process.env.PORT || 3011;
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 app.use(express.static("public"));
 
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
 
-app.listen(PORT, function() {
+const {
+    v4: uuidv4
+} = require('uuid');
+
+let noteArray = [{
+    "title": "Test Title",
+    "text": "Test text"
+}];
+fs.writeFileSync('./db/db.json', JSON.stringify(noteArray));
+
+// app.get('/notes', function (req, res) {
+//     fs.readFile(`./public/notes.html`, (err, data) => {
+//         if (err) throw err;
+//         res.end(data);
+//     });
+// });
+
+app.get('/api/notes', (request, response) => {
+    console.log("\n\nExecuting GET notes request");
+    let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    console.log("\nGET request - Returning notes data: " + JSON.stringify(data));
+
+    response.json(data);
+});
+
+app.get('/notes', function(request, response) {
+    response.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/*', function (request, response) {
+    response.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.post('/api/notes', (request, response) => {
+    const newNote = request.body;
+    console.log("\n\nPOST request - New Note : " + JSON.stringify(newNote));
+    // Assigning unique id from 'uuid' package
+    newNote.id = uuidv4();
+    let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    data.push(newNote);
+    fs.writeFileSync('./db/db.json', JSON.stringify(data));
+    console.log("\nSuccessfully added new note to 'db.json' file.");
+    response.json(data);
+});
+
+//could add app.delete request here with id
+
+app.listen(PORT, function () {
     console.log(`App listening on PORT: ${PORT}`);
 });
